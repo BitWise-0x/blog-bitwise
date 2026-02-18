adfbbc7191f6dbd76d9c7e5fae824e5ee1afa44b
JackieTreeh0rn <1523839+BitWise-0x@users.noreply.github.com>
2026-02-18 15:13:18 -0500

    deploy: theme giscus comments for light/dark mode
    Switch from light/transparent_dark to noborder_light/noborder_dark
    for seamless blending with site background in both themes.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -53,8 +53,8 @@ const siteMetadata = {
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       metadata: '0',
-      theme: 'light',
-      darkTheme: 'transparent_dark',
+      theme: 'noborder_light',
+      darkTheme: 'noborder_dark',
       themeURL: '',
       lang: 'en',
     },
